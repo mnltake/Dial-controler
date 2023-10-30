1235a38718147147f1d07594c1baa9706a6bd480
mnltake <minolu2003@ybb.ne.jp>
2023-10-30 21:48:09 +0900

    速度対応

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,14 @@
 // #include "USBHIDKeyboard.h"
 // USBHIDKeyboard Keyboard;
 
-long oldPosition = -999;
 
+long oldPosition = 0;
+unsigned long lastEncoderUpdateTime = 0;
+int16_t oldEncoderValue = 0;
+int16_t newEncoderValue = 0;
+int16_t newPosition = 0;
+int16_t encoderDelta = 0;
+int16_t encoderChangeThreshold = 5; // 変化量のしきい値
 void setup() {
     auto cfg = M5.config();
     M5Dial.begin(cfg, true, false);
@@ -26,16 +32,44 @@ void setup() {
 
 void loop() {
     M5Dial.update();
-    int16_t newPosition =int16_t(M5Dial.Encoder.read());
-    if (newPosition != oldPosition) {
-        M5Dial.Speaker.tone(8000, 20);
-        M5Dial.Display.clear();
-        oldPosition = newPosition;
-        // Serial.println(newPosition);
-        M5Dial.Display.drawString(String(newPosition),
+    // エンコーダーの値を読み取り
+    newEncoderValue = int16_t(M5Dial.Encoder.read());
+
+    // エンコーダーの変化を時間で追跡
+    unsigned long currentTime = millis();
+    unsigned long deltaTime = currentTime - lastEncoderUpdateTime;
+
+    if (deltaTime >= 50) { // 50ミリ秒ごとに計算
+        // エンコーダーの変化量を計算
+        encoderDelta =     newEncoderValue - oldEncoderValue;
+
+        // 変化量がしきい値を超えていれば値を変更
+        if (abs(encoderDelta) > 0) {
+            // if (abs(encoderDelta) >= 10){
+            //     encoderDelta *= 10;
+            // }else if(abs(encoderDelta) >= 3){
+            //     encoderDelta *= 5;
+            // }else if(abs(encoderDelta) >= 2){
+            //     encoderDelta *= 2;
+            // }
+            newPosition = oldPosition + (encoderDelta*abs(encoderDelta));
+            oldEncoderValue = newEncoderValue;
+            oldPosition = newPosition;
+            lastEncoderUpdateTime = currentTime;
+            M5Dial.Speaker.tone(8000, 20);
+            M5Dial.Display.clear();
+            oldPosition = newPosition;
+            // Serial.println(newPosition);
+            M5Dial.Display.drawString(String(newPosition),
                                   M5Dial.Display.width() / 2,
                                   M5Dial.Display.height() / 2);
+        }
+        
     }
+
+
+
+
     if (M5Dial.BtnA.wasPressed()) {
         Serial.println(newPosition);
         Wire.beginTransmission(I2C_DEV_ADDR);
@@ -47,5 +81,9 @@ void loop() {
     }
     if (M5Dial.BtnA.pressedFor(5000)) {
         M5Dial.Encoder.readAndReset(); 
+        oldEncoderValue = 0;
+        oldPosition =0;
+        lastEncoderUpdateTime = currentTime;
+        delay(100);
     }
 }
